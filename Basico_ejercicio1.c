2c6b7f9635cd21cbe52cd091a855f1561993b4dd
Generarus <ghv@azc.uam.mx>
2021-08-21 01:49:29 -0500

    Ejercicio basico e intermedio
    Se realizaron dos ejercicios: uno de nivel b{asico y otro de nivel intermedio.

---
+++ Basico_ejercicio1.c
@@ -0,0 +1,31 @@
+// Ejercicio 1 nivel básico
+// Imprime un menú y selecciona una opción
+// Autor: GENARO HERNÁNDEZ VALDEZ
+#include <stdio.h>
+/*#include <graphics.h>
+#include <iostream.h>
+#include <linux.h>*/
+
+int main() 
+{
+    int opcion=0;
+    printf("\nOperaciones matemáticas básicas:\n");
+    printf("\n1.- Suma\n\n2.- Resta\n\n3.- Multiplicación\n\n4.- División\n\n5.- Salir\n\n");
+    
+    while (opcion != 5) {   
+        printf("\nElija una opción: ");
+        scanf("%d", &opcion);
+
+        if (opcion==1) printf("\nElegiste la opción %d: suma\n\n", opcion);
+        else if(opcion==2) printf("\nElegiste la opción %d: resta\n\n", opcion);
+            else if(opcion==3) printf("\nElegiste la opción %d: multiplicación\n\n", opcion);
+                else if(opcion==4) printf("\nElegiste la opción %d: división\n\n", opcion);
+                    else if(opcion==5) printf("\nElegiste la opción %d: salir\n\n", opcion);
+                         else printf("\nOpción no valida\n\n");
+    
+        printf("\nPresione cualquier tecla para continuar\n\n");
+        char c=getchar();
+        //clear();
+    }  
+    return 0; 
+}
